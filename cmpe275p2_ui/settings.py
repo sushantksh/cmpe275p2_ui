860d019816ec0a52309e488f2e36a0a7ae9d48b3
Manoj Dhoble <manoj.dhoble@gmail.com>
2013-05-02 22:27:54 -0700

    Integrated Admin Module

--- cmpe275p2_ui/settings.py
+++ cmpe275p2_ui/settings.py
@@ -8,6 +8,18 @@ ADMINS = (('Manoj Dhoble', 'mdhoble@gmail.com'),)
 
 MANAGERS = ADMINS
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/data/sqllite/db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -114,7 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- cmpe275p2_ui/urls.py
+++ cmpe275p2_ui/urls.py
@@ -4,8 +4,8 @@ import mooc.views
 
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
   	 url(r'^$', mooc.views.home),
@@ -57,5 +57,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin', include(admin.site.urls)),
 )
